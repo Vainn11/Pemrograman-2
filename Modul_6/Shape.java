846683251ea5c6f9b709c2b3e28e0299890d3eef
Vainn11 <115350079+Vainn11@users.noreply.github.com>
2023-11-28 19:55:01 +0800

    Add files via upload

---
+++ Modul_6/Shape.java
@@ -0,0 +1,11 @@
+package praktikum5.soal1;
+public abstract class Shape {
+	protected String shapeName;
+	public Shape(String name) {
+		shapeName = name;
+	}
+	public abstract double area();
+	public String toString() {
+		return shapeName;
+	}
+}
---
+++ Modul_6/Sphere.java
@@ -0,0 +1,16 @@
+package praktikum5.soal1;
+public class Sphere extends Shape {
+	private final double radius;
+	public Sphere(double r) {
+		super("Sphere");
+		this.radius = r;
+	}
+	@Override
+	public double area() {
+		return 4 * Math.PI * Math.pow(radius, 2);
+	}
+	@Override
+	public String toString() {
+		return super.toString() + " of radius " + radius;
+	}
+}
